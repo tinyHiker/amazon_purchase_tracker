8582c386528f5a027d77b27d4790fd79c23f1a1c
Taha Iqbal <tiqbal10b@gmail.com>
2024-01-19 12:07:28 -0500

    Add type hinting to Mofifier and Scraper classes

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import sqlite3
 from data_classes import *
 
 
+from typing import Tuple
+
+
 
 class WebsiteNotFound(Exception):
     """Custom exception class for when there is no website that matches a passed URL"""
@@ -29,40 +32,41 @@ class ProductDoesNotExist(Exception):
 class Scraper:
     """Scraper class for scraping the product information off an Amazon product page"""
     
-    def get_content(self, page):
+    def get_content(self, response: requests.Response) -> BeautifulSoup:
         """Parses and returns a beautified version of the given web page's HTML content"""
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(response.content, 'html.parser')
         page = BeautifulSoup(soup.prettify(), 'html.parser')
         return page
     
     
-    def extract_info(self, content):
+    def extract_info(self, content: BeautifulSoup) -> Tuple[str, float, datetime.date]:
         """Extracts the product title and price of the product from the HTML"""
         title = content.find(id="productTitle").get_text().strip()
         whole_price = content.find(class_ = "a-price-whole").get_text()
         whole_price = whole_price.split('.')[0].strip()
         fraction_price = content.find(class_ = "a-price-fraction").get_text().strip()
         final_price = str(whole_price) + '.' + str(fraction_price)
+        final_price = float(final_price)
         today = datetime.date.today()
         
         return (title, final_price, today)
     
     
-    def scrape(self, URL):
+    def scrape(self, URL: str) -> Tuple[str, float, datetime.date]:
         """Takes a URL of the product page and returns information about the product"""
         try:
-            page = requests.get(URL)
+            page : requests.Response = requests.get(URL)
         except:
             raise WebsiteNotFound()
         
         #Extracting the HTML in a 'prettified' format'
-        content = self.get_content(page)
+        content : BeautifulSoup = self.get_content(page)
         #Extracting and returning product info from the HTML
         return self.extract_info(content)
         
         
 
-    def already_visited(self, title, current_user = None):
+    def already_visited(self, title: str, current_user = None) -> bool:
         """Returns True if the active user has already logged this product"""
         with sqlite3.connect('database.db') as conn:  # This will create a file named 'database.db'
             cursor = conn.cursor()
@@ -79,24 +83,24 @@ class Scraper:
 class Modifier:
     """The objects of this class are used to modify the database"""
     
-    def __init__(self, active_user):
+    def __init__(self, active_user: User):
         self.current_user: User = active_user
     
-    def new_user(self, username):
+    def new_user(self, username: str):
         """Creates new user with the currrent loggedin user as the master"""
         self.current_user.create_new_user(username)
         
-    def create_entry(self, title, final_price, today, rating):
-        product = Product.create_new(title, final_price, today, self.current_user, rating)
+    def create_entry(self, title: str, final_price: float, today: datetime.date(), rating: int):
+        product : Product = Product.create_new(title, final_price, today, self.current_user, rating)
             
     
-    def new_product(self, URL, rating):
+    def new_product(self, URL : str, rating: int):
         scraper = Scraper()
         scraped_data = scraper.scrape(URL)
         self.create_entry(title = scraped_data[0], final_price = scraped_data[1], today = scraped_data[2], rating = rating)
         
         
-    def find_product(self, product_code):
+    def find_product(self, product_code: int) -> Product:
         #takes product code and returns product object
         with sqlite3.connect('database.db') as conn:  # This will create a file named 'database.db'
             cursor = conn.cursor()
@@ -110,17 +114,17 @@ class Modifier:
             result = results[0]
             return Product(*result)
         
-    def delete_product(self, product_code):
+    def delete_product(self, product_code : int):
         ##finds product using find_product() method and then calls delete() method on the returned product object
         product = self.find_product(product_code)
         product.delete()
         
-    def change_rating(self, product_code, new_rating):
+    def change_rating(self, product_code : int, new_rating : int):
         #finds product using find_product() method and the calls new_rating on the returned product object
         product = self.find_product(product_code)
         product.change_rating(new_rating)
         
-    def buy(self, product_code):
+    def buy(self, product_code: int):
         product = self.find_product(product_code)
         product.buy()
         
